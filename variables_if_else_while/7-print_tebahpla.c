bc05a4a46bb4ae0974bedbbcd7385e70dc9afbe7
BrayanSA <pg9413041@gmail.com>
2022-10-06 13:24:02 -0700

    My first betty

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char az;
+
+for (az = 'z'; low >= 'a'; low--)
+putchar(az);
+putchar('\n');
+
+return (0);
+}
