42495ee38e82c9c3d8991255c04f11fbb8918bd5
BrayanSA <pg9413041@gmail.com>
2022-10-09 12:34:49 -0700

    My first C functions

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,6 @@
-#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
 * main - Entry point
 *
@@ -6,14 +8,18 @@
 */
 int main(void)
 {
-char *hb = "Holberton";
 
-while (*hb)
-{
-_putchar(*hb);
-hb++;
-}
-_putchar('\n');
+int n;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+
+if (n > 0)
+printf("%d is positive\n", n);
+else if (n == 0)
+printf("%d is zero\n", n);
+else
+printf("%d is negative\n", n);
 
 return (0);
 }
