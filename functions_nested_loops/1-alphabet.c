919dff24a4f1a0c86ac3ed6c4fc21733e91a3eda
BrayanSA <pg9413041@gmail.com>
2022-10-09 13:22:34 -0700

    My first C functions

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* print_alphabet - a function that prints the alphabet, in lowercase
+* print_alphabet - print the alphabet
 *
-* Return: Always 0 (Succes)
+* Return: Always success
 */
 void print_alphabet(void)
 {
