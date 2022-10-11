5bc10986128ca826c248e28abe9a8025330cf123
BrayanSA <pg9413041@gmail.com>
2022-10-11 13:59:15 -0700

    My first C pointers

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+* swap_int - Swaps the values of teo integers
+*
+* @a: An integer to swap
+* @b: Another integer to swap
+*
+* Return: void
+*/
 void swap_int(int *a, int *b)
 {
 int aux;
