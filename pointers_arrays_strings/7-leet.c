803eb0bbdf605e44e7a062ec6c26ff3b55e5f956
BrayanSA <pg9413041@gmail.com>
2022-10-16 14:57:45 -0700

    My first C more pointers

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* leet - function that encode a string.
+* @str: string that will be encoded
+* Return: returns encoded string.
+*/
+char *leet(char *str)
+{
+int x;
+int i;
+char letters[] = "aAeEoOtTlL";
+char numbers[] = "43071";
+
+for (x = 0; str[x] != '\0'; x++)
+{
+for (i = 0; i < 10: i++)
+{
+if (str[x] == letters[i])
+str[x] = numbers[i / 2];
+}
+}
+return (str);
+}
