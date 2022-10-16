58f082b1b72df2e0c929d900d7150c37fa08280f
BrayanSA <pg9413041@gmail.com>
2022-10-16 15:07:59 -0700

    My first C more pointers

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -6,27 +6,31 @@
 */
 char *cap_string(char *str)
 {
-int index = 0;
+int n = 0;
 
-while (str[++index])
+while (str[n])
 {
-while (!(str[index] >= 'a' && str[index] <= 'z'))
-index++;
+while (!(str[n] >= 'a' && str[n] <= 'z'))
+n++;
 
-if (str[index - 1] == ' ' ||
-str[index - 1] == '\t' ||
-str[index - 1] == '\n' ||
-str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '"' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}')
-str[index] -= 32;
+if (str[n - 1] == ' ' ||
+str[n - 1] == '\t' ||
+str[n - 1] == '\n' ||
+str[n - 1] == ',' ||
+str[n - 1] == ';' ||
+str[n - 1] == '.' ||
+str[n - 1] == '!' ||
+str[n - 1] == '?' ||
+str[n - 1] == '"' ||
+str[n - 1] == '(' ||
+str[n - 1] == ')' ||
+str[n - 1] == '{' ||
+str[n - 1] == '}' ||
+n == 0
+str[n] -= 32;
+
+n++;
 }
+
 return (str);
 }
