760eddc5cba49c0f5b6242def0f3bae181876f79
BrayanSA <pg9413041@gmail.com>
2022-10-16 18:55:41 -0700

    My first C Even more pointer

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@ int b, sum1 = 0, sum2 = 0;
 for (b = 0; b < size; b++)
 {
 sum1 += a[(size + 1) * b];
- sum2 += a[(size - 1) * (b + 1)];
+sum2 += a[(size - 1) * (b + 1)];
 }
 printf("%d, %d\n", sum1, sum2);
 }
