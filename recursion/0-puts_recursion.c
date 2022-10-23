f4ec3a0f582c101fb1ee5ddc7eeee3c15a873c17
BrayanSA <pg9413041@gmail.com>
2022-10-23 16:06:06 -0700

    My first C recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* _puts_recursion - Prints a string, followed by a new line.
+* @s: Pointer to string being evaluated.
+*/
+void _puts_recursion(char *s)
+{
+if (!*s)
+{
+_putchar;
+}
+_puthcar(*s);
+_puts_recursion(s + 1);
+}
