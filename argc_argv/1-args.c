e476dbec4a6a3f58964539528444a041a5adfe42
BrayanSA <pg9413041@gmail.com>
2022-10-24 10:21:48 -0700

    My first C argc, argv

---
+++ argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* main - Prints the number of arguments.
+* @argc: Number of command line arguments.
+* @argv: Array containing command line arguments.
+* Return: 0
+*/
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%i\n", argc - 1);
+return (0);
+}
