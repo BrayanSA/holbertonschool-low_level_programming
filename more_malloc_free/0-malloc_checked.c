4fe0f59e5d4e9081545832b692f2a49bab78bab9
BrayanSA <pg9413041@gmail.com>
2022-10-29 13:43:43 -0700

    My first C malloc, free

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* malloc_checked - Allocates memory using malloc
+* @b: Amount of memory being allocated
+* Return: Pointer to allocated memory
+*/
+void  *malloc_checked(unsigned int b)
+{
+void *n;
+
+n = malloc(b);
+if (!n)
+exit(98);
+return (n);
+}
