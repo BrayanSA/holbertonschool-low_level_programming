796739cd81d69542e92343f5b3a4b5c4e2ab5e90
BrayanSA <pg9413041@gmail.com>
2022-10-30 18:32:21 -0700

    My first C more malloc, free

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -1,37 +1,23 @@
 #include "main.h"
 #include <stdlib.h>
 /**
-* string_nconcat - Concatenates two strings
-* @s1: First string
-* @s2: Second string
-* @n: String bytes to be copied
-* Return: Pointer to concatenated string, NULL if fail
+* array_range - Creates an array of integers
+* @min: Minimum value
+* @max: Maximum value
+* Return: Pointer to newly created array
 */
-char *string_nconcat(char *s1, char *s2, unsigned int n)
+int *array_range(int min, int max)
 {
-char *str;
-int len1;
-unsigned int i = 0;
+int *range, len;
+int i = 0;
 
-if (!s1)
-s1 = "";
-if (!s2)
-s2 = "";
-while (*(s1 + i))
-++i;
-len1 = i;
-i = 0;
-while (*(s2 + i))
-++i;
-if (n >= i)
-n = i;
-str = malloc(sizeof(char) * (len1 + n + 1));
-if (!str)
+if (min > max)
 return (NULL);
-for (i = 0; *s1; ++i)
-*(str + i) = *(s1++);
-for (; i < len1 + n; ++i)
-*(str + i) = *(s2++);
-*(str + i) = '\0';
-return (str);
+len = max - min + 1;
+range = malloc(sizeof(int) * len);
+if (!range)
+return (NULL);
+while (len--)
+*(range + i++) = min++;
+return (range);
 }
