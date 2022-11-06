8372cf954541d146573636e95dac4a614f8b3c3b
BrayanSA <pg9413041@gmail.com>
2022-11-06 12:26:47 -0800

    My first C function pointers

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,11 +1,11 @@
 #include "function_pointers.h"
 /**
-* array_function - Executes function as parameter on each element of array.
+* array_iterator - Executes function as parameter on each element of array.
 * @array: Pointer to array.
 * @size: Size of array.
 * @action: Pointer to function code.
 */
-void array_function(int *array, size_t size, void (*action)(int))
+void array_iterator(int *array, size_t size, void (*action)(int))
 {
 size_t i = 0;
 
